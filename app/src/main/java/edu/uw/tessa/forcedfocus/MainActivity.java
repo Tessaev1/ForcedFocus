9fa7c0984789f0a600995fee6b8e04e5b32efd26
Cat <cathej6@gmail.com>
2017-02-23 15:07:28 -0800

    Got code to spam messages and send a simple text working. Now just need to work on accessing contacts and using it to send the messages.

--- app/src/main/java/edu/uw/tessa/forcedfocus/MainActivity.java
+++ app/src/main/java/edu/uw/tessa/forcedfocus/MainActivity.java
@@ -1,29 +1,49 @@
 package edu.uw.tessa.forcedfocus;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.telephony.SmsManager;
 
 import com.facebook.AccessToken;
-import com.facebook.login.LoginManager;
-import com.facebook.login.widget.LoginButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.R.attr.inset;
+import static android.R.attr.phoneNumber;
+import static android.R.id.message;
 
 public class MainActivity extends AppCompatActivity {
     private AccessToken userToken = AccessToken.getCurrentAccessToken();
 
     public static final String TAG = "MainActivity";
 
+    public int index = 0;
+
+    public String[] insults = {
+      "You failed", "Failure", "Loser", "LOSER!!", "Mwahahaha!"
+    };
+    public Timer timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (!this.isLoggedIn()) {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
@@ -31,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             final TextView currentUser = (TextView) findViewById(R.id.currentUser);
             currentUser.setText("Current user id: " + this.userToken.getUserId());
         }
+        //sendBadToasts();
+        //sendBadText();
     }
 
     public boolean isLoggedIn() {
@@ -48,4 +70,62 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, PreferencesActivity.class);
         startActivity(intent);
     }
+
+    // Sends mean toasts every 2 seconds for one minute.
+    public void sendBadToasts() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        index++;
+                        final Toast toast = Toast.makeText(
+                                getApplicationContext(),  insults[index % insults.length],
+                                Toast.LENGTH_SHORT);
+                        toast.show();
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                toast.cancel();
+                            }
+
+                        }, 2000);
+
+                    }
+                });
+            }
+        }, 0, 1000);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                timer.cancel();
+            }
+        }, 60000);
+    }
+
+
+    // Sends this text message to a specific phoneNumber
+    // get Conacts is to be added soon.
+    public void sendBadText() {
+        String phoneNumber = "3605846299";
+        String message = "I'm suck at Focusing... ";
+
+        //SmsManager sms = SmsManager.getDefault();
+        //sms.sendTextMessage(phoneNumber, null, message, null, null);
+
+        Toast toast = Toast.makeText(
+                getApplicationContext(),  phoneNumber + ": " + message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
 }
